eec54ef9411ac546a8e0ac3044a427e831e358ff
Wiam <wiamnasr@gmail.com>
2021-12-04 07:32:18 +0000

    used useContext hook instead of AuthContext Wrapper, same result, more elegant solution

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,35 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  // storing in a ctx const to make my code down there work again, this is a more elegant solution!
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href='/'>Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    // <AuthContext.Consumer>
+    //   {(ctx) => {
+    // return (
+
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href='/'>Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+
+    // );
+    //   }}
+    // </AuthContext.Consumer>
   );
 };
 
